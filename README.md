fa4badcd91901f19c0a68b41c41990ca85225650
Abner Oliveira <abner.oliveira@serpro.gov.br>
2021-02-18 08:12:32 -0300

    separated jvmci related jars into the .jvmci folder and renamed jars folder to .lib. Changed to support debug also in regular (not GraalVM)  Java SDKs.

--- README.md
+++ README.md
@@ -83,18 +83,16 @@ Row 4   ::::    4       |       Zeneth
 
 ## Debug with Chrome Inspector
 
-For debug facilities, it's necessary to use a GraalVM SDK, which
-provides "--inspect" arguments and Chrome Inspector to the Java Runtime.
-
-In order to inspect the script execution through Chrome Inspector, just
-run the script passing the argument `--inspect`:
+In order to inspect the script execution through Chrome Inspector, just run the script using the alias `graaljs_debug`:
 
 ```bash
-graaljs postgres_graal_script.js
+graaljs_debug postgres_graal_script.js
 ```
 
 Then open the url `chrome://inspect` on Chrome or Chromium browser and click in `inspect` under the `GraalVM` label.
 
+
+If you're using the Visual Studio Code Editor, you can also attach to the debug session using the Launcher graaljs-standalone after running the script through the graaljs_debug alias.
 
 
 ## References:
--- setup.sh
+++ setup.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-export JJS_JARSPATH="$PWD/jars"
+export JJS_JARSPATH="$PWD/.libs"
+export JJS_JVMCIPATH="$PWD/.jvmci"
 
 if [ ! -d $JJS_JARSPATH ]; then
-  mkdir $JJS_JARSPATH && \
-  cd $JJS_JARSPATH && \
-  wget https://repo1.maven.org/maven2/com/ibm/icu/icu4j/68.2/icu4j-68.2.jar && \
-  wget https://repo1.maven.org/maven2/org/graalvm/sdk/graal-sdk/21.0.0.2/graal-sdk-21.0.0.2.jar &&  \
-  wget https://repo1.maven.org/maven2/org/graalvm/truffle/truffle-api/21.0.0.2/truffle-api-21.0.0.2.jar && \
-  wget https://repo1.maven.org/maven2/org/graalvm/compiler/compiler/21.0.0.2/compiler-21.0.0.2.jar && \
-  wget https://repo1.maven.org/maven2/org/graalvm/compiler/compiler-management/21.0.0.2/compiler-management-21.0.0.2.jar && \
-  wget https://repo1.maven.org/maven2/org/graalvm/sdk/launcher-common/21.0.0.2/launcher-common-21.0.0.2.jar && \
-  wget https://repo1.maven.org/maven2/org/graalvm/js/js-launcher/21.0.0.2/js-launcher-21.0.0.2.jar && \
-  wget https://repo1.maven.org/maven2/org/graalvm/js/js/21.0.0.2/js-21.0.0.2.jar && \
-  wget https://repo1.maven.org/maven2/org/graalvm/js/js-scriptengine/21.0.0.2/js-scriptengine-21.0.0.2.jar &&
-  wget https://repo1.maven.org/maven2/org/graalvm/regex/regex/21.0.0.2/regex-21.0.0.2.jar && \
-  wget https://repo1.maven.org/maven2/org/graalvm/tools/chromeinspector/21.0.0.2/chromeinspector-21.0.0.2.jar && \
-  wget https://repo1.maven.org/maven2/org/graalvm/tools/profiler/21.0.0.2/profiler-21.0.0.2.jar && \
-  wget https://jdbc.postgresql.org/download/postgresql-42.2.18.jar
+  mkdir $JJS_JARSPATH && mkdir -p .jvmci && \
+  wget -P $JJS_JVMCIPATH https://repo1.maven.org/maven2/org/graalvm/sdk/graal-sdk/21.0.0.2/graal-sdk-21.0.0.2.jar &&  \
+  wget -P $JJS_JVMCIPATH https://repo1.maven.org/maven2/org/graalvm/truffle/truffle-api/21.0.0.2/truffle-api-21.0.0.2.jar && \
+  wget -P $JJS_JVMCIPATH https://repo1.maven.org/maven2/org/graalvm/compiler/compiler/21.0.0.2/compiler-21.0.0.2.jar && \
+  wget -P $JJS_JVMCIPATH https://repo1.maven.org/maven2/org/graalvm/compiler/compiler-management/21.0.0.2/compiler-management-21.0.0.2.jar && \
+  wget -P $JJS_JARSPATH https://repo1.maven.org/maven2/com/ibm/icu/icu4j/68.2/icu4j-68.2.jar && \
+  wget -P $JJS_JARSPATH https://repo1.maven.org/maven2/org/graalvm/sdk/launcher-common/21.0.0.2/launcher-common-21.0.0.2.jar && \
+  wget -P $JJS_JARSPATH https://repo1.maven.org/maven2/org/graalvm/js/js-launcher/21.0.0.2/js-launcher-21.0.0.2.jar && \
+  wget -P $JJS_JARSPATH https://repo1.maven.org/maven2/org/graalvm/js/js/21.0.0.2/js-21.0.0.2.jar && \
+  wget -P $JJS_JARSPATH https://repo1.maven.org/maven2/org/graalvm/js/js-scriptengine/21.0.0.2/js-scriptengine-21.0.0.2.jar &&
+  wget -P $JJS_JARSPATH https://repo1.maven.org/maven2/org/graalvm/regex/regex/21.0.0.2/regex-21.0.0.2.jar && \
+  wget -P $JJS_JARSPATH https://repo1.maven.org/maven2/org/graalvm/tools/chromeinspector/21.0.0.2/chromeinspector-21.0.0.2.jar && \
+  wget -P $JJS_JARSPATH https://repo1.maven.org/maven2/org/graalvm/tools/profiler/21.0.0.2/profiler-21.0.0.2.jar && \
+  wget -P $JJS_JARSPATH https://jdbc.postgresql.org/download/postgresql-42.2.18.jar
 fi
 
 
-alias graaljs="java -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler --module-path=$JJS_JARSPATH/graal-sdk-21.0.0.2.jar:$JJS_JARSPATH/truffle-api-21.0.0.2.jar --upgrade-module-path=$JJS_JARSPATH/compiler-21.0.0.2.jar:$JJS_JARSPATH/compiler-management-21.0.0.2.jar -cp $JJS_JARSPATH/launcher-common-21.0.0.2.jar:$JJS_JARSPATH/js-launcher-21.0.0.2.jar:$JJS_JARSPATH/js-21.0.0.2.jar:$JJS_JARSPATH/truffle-api-21.0.0.2.jar:$JJS_JARSPATH/graal-sdk-21.0.0.2.jar:$JJS_JARSPATH/js-scriptengine-21.0.0.2.jar:$JJS_JARSPATH/regex-21.0.0.2.jar:$JJS_JARSPATH/chrome-inspector-21.0.0.2.jar:$JJS_JARSPATH/icu4j-68.2.jar:$JJS_JARSPATH/postgresql-42.2.18.jar -Dpostgres.port=${POSTGRES_PORT:-5432} com.oracle.truffle.js.shell.JSLauncher"
+JAVA_CLASSPATH="$JJS_JARSPATH/launcher-common-21.0.0.2.jar:$JJS_JARSPATH/js-launcher-21.0.0.2.jar:$JJS_JARSPATH/js-21.0.0.2.jar:$JJS_JVMCIPATH/truffle-api-21.0.0.2.jar:$JJS_JVMCIPATH/compiler-management-21.0.0.2.jar:$JJS_JVMCIPATH/graal-sdk-21.0.0.2.jar:$JJS_JARSPATH/js-scriptengine-21.0.0.2.jar:$JJS_JARSPATH/regex-21.0.0.2.jar:$JJS_JARSPATH/chrome-inspector-21.0.0.2.jar:$JJS_JARSPATH/icu4j-68.2.jar:$JJS_JARSPATH/postgresql-42.2.18.jar"
+JVMCI_CLASSPATH="$JJS_JVMCIPATH:$JJS_JARSPATH"
+# -Dpolyglot.inspect=9300
+MODULES_READS="--add-reads com.oracle.truffle.tools.chromeinspector=java.logging"
+MODULES_EXPORTS="--add-exports com.oracle.truffle.tools.profiler/com.oracle.truffle.tools.utils.json=com.oracle.truffle.tools.chromeinspector"
+#MODULES_PATCH="--add-modules ALL-MODULE-PATH"
+alias graaljs="java $MODULES_READS $MODULES_EXPORTS -XX:+IgnoreUnrecognizedVMOptions --module-path=$JVMCI_CLASSPATH -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler  --upgrade-module-path=$JJS_JVMCIPATH/compiler-21.0.0.2.jar:$JJS_JVMCIPATH/compiler-management-21.0.0.2.jar -Dpostgres.port=${POSTGRES_PORT:-5432}  -cp $JAVA_CLASSPATH com.oracle.truffle.js.shell.JSLauncher"
+alias graaljs_debug="java $MODULES_READS $MODULES_EXPORTS -XX:+IgnoreUnrecognizedVMOptions -Dpolyglot.inspect=9300 --module-path=$JVMCI_CLASSPATH -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler  --upgrade-module-path=$JJS_JVMCIPATH/compiler-21.0.0.2.jar:$JJS_JVMCIPATH/compiler-management-21.0.0.2.jar -Dpostgres.port=${POSTGRES_PORT:-5432}  -cp $JAVA_CLASSPATH com.oracle.truffle.js.shell.JSLauncher"
 alias startpg="docker run --name pggraal -d --rm -it -v $PWD/postgresql:/docker-entrypoint-initdb.d -e  POSTGRES_USER=pguser -e POSTGRES_DB=pgdb  -e POSTGRES_PASSWORD=pgpassword -p ${POSTGRES_PORT:-5432}:5432 postgres:12-alpine"
 alias stoppg="docker stop pggraal"
